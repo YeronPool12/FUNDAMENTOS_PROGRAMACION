5b943b264f3331eb28f9d2cee16501230331888e
Yeron Pool Cuero Montaño <92317084+YeronPool12@users.noreply.github.com>
2021-12-14 15:47:26 -0500

    Actividad-C2

---
+++ Actividad-C2/CueroYeron/SumaSimple.cpp
@@ -0,0 +1,28 @@
+/*SUMA SIMPLE*/
+
+#include<iostream>
+
+using namespace std; 
+
+int main()
+{
+	float x,y,z;
+	
+	cout<<"INGRESE EL NUMERO DENOMINADO CON LA VARIABLE X : ";
+	cin>>x;
+	
+	cout<<"INGRESE EL NUMERO DENOMINADO CON LA VARIABLE Y : ";
+	cin>>y;
+	
+	z=x+y;
+	
+	cout<<"EL RESULTADO DE LA RESPECTIVA SUMA ES : "<<z<<endl;
+	
+	
+	return 0;
+	
+}
+
+
+
+
