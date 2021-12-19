0cfb87be7a5861c491f2c2dd87b27a1d41477083
Nicole Karen Banchon <92698285+Nicole-Karen@users.noreply.github.com>
2021-12-18 22:22:05 -0500

    Add files via upload

---
+++ Actividad-C2/BanchonNicole/SumaDosNumeros/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*Nicole Karen Banchón Angulo*/
+using namespace std;
+int main() {
+	//Se declaran las 3 variables a usar.
+	float x, y, z;
+	//Se muestra un mensaje para pedir los numeros
+	cout << "Ingrese el numero x: ";
+	cin >> x;
+	cout << "Ingrese el numero y: ";
+	cin >> y;
+	//Se procede a la realización de la suma
+	z = x + y;
+	//Se mostrarán los resultados pertinentes
+	cout << "El resultado de la suma es: " << z << endl;
+	return 0;
+}
