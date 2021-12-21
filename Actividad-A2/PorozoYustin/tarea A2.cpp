5da2cc8c59e085dc3f494eefca4106f7bfd7ef6e
yustinp <92316142+yporozo@users.noreply.github.com>
2021-12-20 21:42:39 -0500

    Add files via upload

---
+++ Actividad-A2/PorozoYustin/tarea A2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+}
+float x, C1=01+30, Ca2=0, Cr3=0;
+do{
+cout<<"ingrese el promedio: "; cin>>x;
+c1=c1+1;
+if(x>7){
+	ca2=ca2+1;
+}else{
+	if(x<5){
+		cr3=cr3+1;
+	}else{}
+}
+
+}while(c1<1);
+
+cout<<"aprueban" <<ca2<<endl;
+cout<<"reprueban" <<cr3<<endl;
+return(0) ;
+
+}
---
+++ Actividad-A2/PorozoYustin/tarea a2....cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float X, C1=01, S2=0, Si3=o, Se4=0;
+cout<<"cuantos valores va ingresar: "
+; cin>>l
+do{
+cout<<"ingrese los valores: "; cin>>x;
+c1=c1*1;
+s2=s2+x;
+if(x>0){
+	si3=si3+x;
+}else{
+	se4=se4+x;
+}
+}while(c<1);
+cout<<"el saldo de egreso es: "
+<<se4<<endl;
+cout<<"el saldo de ingreso es: "
+<<si3<<endl;
+cout<<"el saldo total es: " <<s1<<endl;
+return(0);
+}
